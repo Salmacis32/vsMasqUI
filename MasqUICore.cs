721b9d1a2523d7ff6a3cfba838718cc372658371
Mrs. Mercedes <mayf218@outlook.com>
2025-08-21 18:32:00 -0400

    Tinting the Level Up UI based on Co op Color

---
+++ MasqUICore.cs
@@ -0,0 +1,23 @@
+﻿using MasqUI.Patches;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(MasqUI.MasqUICore), "MasqUI", "1.0.0", "Mercy", null)]
+[assembly: MelonGame("poncle", "Vampire Survivors")]
+
+namespace MasqUI
+{
+    public class MasqUICore : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+
+        public override void OnDeinitializeMelon()
+        {
+            GameManagerPatches.Deinitialize();
+            OptionsControllerPatches.Deinitialize();
+            ButtonNavigatorPatches.Deinitialize();
+        }
+    }
+}
---
+++ OptionsControllerPatches.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using Il2CppVampireSurvivors.Data;
+using Il2CppVampireSurvivors.UI;
+
+namespace MasqUI.Patches
+{
+    /// <summary>
+    /// Harmony patches for the GameManager class
+    /// </summary>
+    [HarmonyPatch(typeof(OptionsController))]
+    public static class OptionsControllerPatches
+    {
+        public static void Deinitialize()
+        {
+        }
+
+        [HarmonyPatch(nameof(OptionsController.SetPlayerColourIndex))]
+        [HarmonyPostfix]
+        public static void SetColor(OptionsController __instance, int playerIndex)
+        {
+            if (!CheckConfigs(__instance, out PlayerOptionsData data)) return;
+            GameManagerPatches.SetColorForPlayerIndex(playerIndex, data);
+        }
+
+        private static bool CheckConfigs(OptionsController __instance, out PlayerOptionsData data)
+        {
+            data = __instance._playerOptions.CurrentAdventureSaveData;
+            if (data != null)
+            {
+                return true;
+            }
+            else if (__instance._playerOptions.MainGameConfig != null)
+            {
+                data = __instance._playerOptions.MainGameConfig;
+                return true;
+            }
+            return false;
+        }
+
+        [HarmonyPatch(nameof(OptionsController.ToggleTintUISelection))]
+        [HarmonyPostfix]
+        public static void ToggleTint(OptionsController __instance)
+        {
+            if (!CheckConfigs(__instance, out PlayerOptionsData data)) return;
+            GameManagerPatches.LoadPlayerColors(data);
+        }
+    }
+}
